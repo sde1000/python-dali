772cff3e00ed9b9a7048f0610c83da7c8d7bfed9
Stephen Early <steve@greenend.org.uk>
2015-03-24 16:37:48 +0000

    WIP: object model for devices
    Very early work on an object model for DALI buses and devices.  Only
    committed because it's been sitting in my working directory for
    months...

---
+++ dali/device.py
@@ -0,0 +1,25 @@
+from . import command
+from .address import Short
+
+# descriptor classes for DALI device attributes goes here...
+# XXX
+
+
+
+class Device(object):
+    """Any DALI slave device that has been configured with a short
+    address.
+
+    """
+    def __init__(self,address,name=None,bus=None):
+        if not isinstance(address,int) or address<0 or address>63:
+            raise ValueError("address must be an integer in the range 0..63")
+        self.address=address
+        self._addressobj=Short(address)
+        self.bus=None
+        if bus: self.bind(bus)
+    def bind(self,bus):
+        """Bind this device object to a particular DALI bus.
+
+        """
+        bus.add_device(self)
