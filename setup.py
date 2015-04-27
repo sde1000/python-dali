441fc40a69ec0087dc9c7d8664d87f12e9ebdd20
Stephen Early <steve@greenend.org.uk>
2015-04-27 18:21:14 +0100

    Build system
    I need to get a package into my private repo quickly to install it at a
    site... build system might be a bit rough-and-ready for now!

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dali',
+      version='0.1',
+      description='Interface to DALI lighting systems',
+      author='Stephen Early',
+      author_email='steve@greenend.org.uk',
+      url='https://github.com/sde1000/python-dali',
+      packages=['dali'],
+      )
