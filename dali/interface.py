0a6b2c536b97573e3b435c2dcf57a1bfa8945e4d
Stephen Early <steve@greenend.org.uk>
2015-08-08 14:38:46 +0100

    Add context manager and rename daliserver to DaliServer

--- dali/interface.py
+++ dali/interface.py
@@ -2,13 +2,15 @@ from __future__ import print_function
 import struct
 import socket
 import logging
+from .command import Command
+from .command import Response
 
 
 class CommunicationError(Exception):
     pass
 
 
-class daliserver(object):
+class DaliServer(object):
     """Communicate with daliserver
     (https://github.com/onitake/daliserver)
 
@@ -17,23 +19,50 @@ class daliserver(object):
 
     """
 
-    def __init__(self, host="localhost", port=55825):
+    def __init__(self, host="localhost", port=55825,
+                 multiple_frames_per_connection=False):
         self._target = (host, port)
+        self._s = None
+        self._multiple_frames_per_connection = multiple_frames_per_connection
+
+    def __enter__(self):
+        if self._multiple_frames_per_connection:
+            self._s = socket.create_connection(self._target)
+        return self
+
+    def __exit__(self, *vpass):
+        print(vpass)
+        if self._multiple_frames_per_connection:
+            self._s.close()
+            self._s = None
 
     def send(self, command):
-        message = struct.pack("BBBB", 2, 0, *command.command)
-        s = socket.create_connection(self._target)
-        if self._verbose:
-            print("{}{}".format(unicode(command), " (twice)" if command._isconfig else ""))
+        if self._s:
+            s = self._s
+        else:
+            s = socket.create_connection(self._target)
+
+        assert isinstance(command, Command)
+        message = command.pack
+
+        logging.info(u"command: {}{}".format(
+            command, " (twice)" if command._isconfig else ""))
+
+        # Set a default result which may be used if the first send fails
         result = "\x02\xff\x00\x00"
+
         try:
             s.send(message)
             result = s.recv(4)
             if command._isconfig:
                 s.send(message)
                 result = s.recv(4)
+        except:
+            raise
         finally:
-            s.close()
+            if not self._s:
+                s.close()
+
         ver, status, rval, pad = struct.unpack("BBBB", result)
         response = None
         if command._response:
@@ -42,15 +71,17 @@ class daliserver(object):
             elif status == 1:
                 response = command._response(rval)
             elif status == 255:
-                # This is "failure" - daliserver seems to be reporting this
-                # for a garbled response when several ballasts reply.
+                # This is "failure" - daliserver seems to be reporting
+                # this for a garbled response when several ballasts
+                # reply.  It should be interpreted as "Yes".
                 response = command._response(255)
             else:
                 raise CommunicationError("status was %d" % status)
-        if self._verbose:
+
             if response:
-                print("  -> {}".format(unicode(response)))
+                logging.info(u"  -> {}".format(response))
+
         return response
 
 
-__all__ = ["daliserver"]
+__all__ = ["DaliServer"]
