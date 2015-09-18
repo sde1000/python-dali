1c73679b1fb6746d0d74a1abd4748b6bf21e637b
Stephen Early <steve@greenend.org.uk>
2015-09-18 18:19:12 +0100

    New "Frame" classes
    DALI frames can have an arbitrary number of bits, not just 8, 16 or 24.
    Instead of storing DALI frames in sequences of bytes, use this class and
    convert to sequences of bytes where necessary.
    
    These classes support various bit-manipulation operations to make
    writing code that deals with DALI frames clearer.

---
+++ dali/frame.py
@@ -0,0 +1,224 @@
+from __future__ import unicode_literals
+
+import struct
+
+_bad_init_data = TypeError(
+    "data must be a sequence of integers all in the range 0..255 or an integer")
+
+class Frame(object):
+    """A DALI frame.
+
+    A Frame consists of one start bit, n data bits, and one stop
+    condition.  The most significant bit is always transmitted first.
+    
+    Instances of this object are mutable.
+    """
+    def __init__(self, bits, data=0):
+        """Initialise a Frame with the supplied number of data bits.
+
+        :parameter bits: the number of data bits in the Frame
+        :parameter data: initial data for the Frame as an integer or
+        an iterable sequence of integers
+        """
+        if not isinstance(bits, int):
+            raise TypeError(
+                "Number of bits must be an integer")
+        if bits < 1:
+            raise ValueError(
+                "Frames must contain at least 1 data bit")
+        self._bits = bits
+        if isinstance(data, int) or isinstance(data, long):
+            self._data = data
+        else:
+            d=0
+            for b in data:
+                if not isinstance(b, int):
+                    raise _bad_init_data
+                if b < 0 or b > 255:
+                    raise _bad_init_data
+                d = (d << 8) | b
+            self._data = d
+        if self._data < 0:
+            raise ValueError(
+                "Initial data must not be negative")
+        if self._data.bit_length() > bits:
+            raise ValueError(
+                "Initial data will not fit in {} bits".format(bits))
+        self._error = False
+    @property
+    def error(self):
+        """Frame was received with a framing error."""
+        return self._error
+    def __len__(self):
+        return self._bits
+    def __eq__(self, other):
+        try:
+            return self._bits == other._bits and self._data == other._data
+        except:
+            return False
+    def __ne__(self, other):
+        try:
+            return self._bits != other._bits or self._data != other._data
+        except:
+            return True
+    def _readslice(self, key):
+        """Check that a slice is valid, return indices
+
+        The slice must have indices that are integers.  The indices
+        must be in the range 0..(len(self)-1).
+        """
+        if not isinstance(key.start, int) or not isinstance(key.stop, int):
+            raise TypeError("slice indices must be integers")
+        if key.step not in (None, 1):
+            raise TypeError("slice with step not supported")
+        hi = max(key.start,key.stop)
+        lo = min(key.start,key.stop)
+        if hi < 0 or lo < 0:
+            raise IndexError("slice indices must be >= 0")
+        if hi >= self._bits or lo >= self._bits:
+            raise IndexError("slice index out of range")
+        return hi, lo
+    def __getitem__(self, key):
+        """Read a bit or group of bits from the frame
+
+        If the key is an integer, return that bit as True or False or
+        raise IndexError if the key is out of bounds.
+
+        If the key is a slice, return that slice as an integer or
+        raise IndexError if out of bounds.  We abuse the slice
+        mechanism slightly such that slice(5,7) and slice(7,5) are
+        treated the same.  Slices with a step or a negative index are
+        not supported.
+        """
+        if isinstance(key, slice):
+            hi, lo = self._readslice(key)
+            d = self._data >> lo
+            return d & ((1 << (hi + 1 - lo)) - 1)
+        elif isinstance(key, int):
+            if key < 0 or key >= self._bits:
+                raise IndexError("index out of range")
+            return (self._data & (1 << key)) != 0
+        raise TypeError
+    def __setitem__(self, key, value):
+        """Write a bit or a group of bits to the frame
+
+        If the key is an integer, set that bit to the truth value of
+        value or raise IndexError if the key is out of bounds.
+
+        If the key is a slice, value must be an integer that fits
+        within the slice; set that slice to value or raise IndexError
+        if out of bounds.  We abuse the slice mechanism slightly such
+        that slice(5,7) and slice(7,5) are treated the same.  Slices
+        with a step or a negative index are not supported.
+        """
+        if isinstance(key, slice):
+            hi, lo = self._readslice(key)
+            if not (isinstance(value, int) or isinstance(value, long)):
+                raise TypeError("value must be an integer")
+            if value.bit_length() > (hi + 1 - lo):
+                raise ValueError("value will not fit in supplied slice")
+            if value < 0:
+                raise ValueError("value must not be negative")
+            template = ((1 << hi + 1 - lo) - 1) << lo
+            mask = ((1 << self._bits) - 1) ^ template
+            self._data = self._data & mask | (value << lo)
+        elif isinstance(key, int):
+            if key < 0 or key >= self._bits:
+                raise IndexError("index out of range")
+            if value:
+                self._data = self._data | (1 << key)
+            else:
+                self._data = self._data & (((1 << self._bits) - 1) ^ (1 << key))
+        else:
+            raise TypeError
+    def __contains__(self,item):
+        if item is True:
+            return self._data != 0
+        if item is False:
+            return self._data != (1 << self._bits) - 1
+        return False
+    def __add__(self,other):
+        try:
+            return Frame(self._bits + other._bits,
+                         self._data << other._bits | other._data)
+        except:
+            raise TypeError("Frame can only be added to another Frame")
+    @property
+    def as_integer(self):
+        """The contents of the frame represented as an integer."""
+        return self._data
+    @property
+    def as_byte_sequence(self):
+        """The contents of the frame represented as a sequence.
+
+        Returns a sequence of integers each in the range 0..255
+        representing the data in the frame, with the most-significant
+        bits first.  If the frame is not an exact multiple of 8 bits
+        long, the first element in the sequence contains fewer than 8
+        bits.
+        """
+        remaining=len(self)
+        l=[]
+        d=self._data
+        while remaining>0:
+            l.append(d & 0xff)
+            d = d >> 8
+            remaining = remaining - 8
+        l.reverse()
+        return l
+    @property
+    def pack(self):
+        """The contents of the frame represented as a byte string.
+
+        If the frame is not an exact multiple of 8 bits long, the
+        first byte in the string will contain fewer than 8 bits.
+        """
+        s=self.as_byte_sequence
+        return struct.pack("B"*len(s),*s)
+    def __unicode__(self):
+        return "{}({},{})".format(self.__class__.__name__, len(self),
+                                  self.as_byte_sequence)
+
+class ForwardFrame(Frame):
+    """A frame that can be transmitted as a command or message.
+
+    Forward Frames with 16 data bits are used to communicate with
+    control gear conformant to IEC 62386-102.  Forward Frames with 24
+    data bits are used by and to communicate with control gear
+    conformant to IEC 62386-103.  Forward Frames with 20 or 32 data
+    bits are reserved and shall not be used.  Forward Frames with any
+    other number of data bits are proprietary.
+    """
+    @property
+    def is_reserved(self):
+        return len(self) == 20 or len(self) == 32
+    @property
+    def is_proprietary(self):
+        return len(self) not in (16, 20, 24, 32)
+
+class BackwardFrame(Frame):
+    """A response to a forward frame.
+
+    Backward Frames always have 8 data bits and are used only as
+    answers to Forward Frames.
+
+    In some circumstances it is normal for a backward frame to be
+    received with frame size or bit timing violations, when more than
+    one unit responds to a forward frame.  In this case, create a
+    BackwardFrameError instead.
+    """
+    def __init__(self, data):
+        Frame.__init__(8, data)
+        self._error = False
+
+class BackwardFrameError(BackwardFrame):
+    """A response to a forward frame received with a framing error.
+
+    This occurs when multiple devices respond to a forward frame at
+    once.  This is normal when a Query command with a yes/no response
+    is addressed to a group or broadcast address.  It shall be
+    interpreted as "more than one device responded Yes".
+    """
+    def __init__(self,data):
+        BackwardFrame.__init__(self,data)
+        self._error = True
---
+++ dali/test_frame.py
@@ -0,0 +1,141 @@
+try:
+    from dali import frame
+except:
+    import os
+    import sys
+
+    PACKAGE_PARENT = '..'
+    SCRIPT_DIR = os.path.dirname(
+        os.path.realpath(os.path.join(os.getcwd(),
+                         os.path.expanduser(__file__))))
+    sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+    from dali import frame
+
+import unittest
+
+class TestFrame(unittest.TestCase):
+    def test_frame_bits(self):
+        "frames can only be initialised with an appropriate number of bits"
+        self.assertRaises(TypeError, frame.Frame, None)
+        self.assertRaises(TypeError, frame.Frame, "wibble")
+        self.assertRaises(TypeError, frame.Frame, 16.0)
+        self.assertRaises(ValueError, frame.Frame, 0)
+        self.assertRaises(ValueError, frame.Frame, 16, 0x1ffff)
+        self.assertRaises(ValueError, frame.Frame, 16, -1)
+        self.assertEqual(len(frame.Frame(16, 0xffff)), 16)
+        self.assertEqual(len(frame.Frame(256, 1 << 255)), 256)
+
+    def test_frame_init_data(self):
+        "frames can be initialised with integer or iterable data"
+        self.assertEqual(frame.Frame(16, 0xffff), frame.Frame(16, (0xff, 0xff)))
+        self.assertNotEqual(frame.Frame(16, 0xffff),
+                            frame.Frame(16, (0xff, 0xfe)))
+
+    def test_comparisons(self):
+        "frame comparisons"
+        self.assertNotEqual(frame.Frame(24, 1), 1)
+        self.assertNotEqual(frame.Frame(24, 1), frame.Frame(16, 1))
+        self.assertEqual(frame.Frame(1),frame.Frame(1))
+        self.assertEqual(frame.Frame(1) == frame.Frame(2), False)
+        self.assertEqual(frame.Frame(1) != frame.Frame(2), True)
+
+    def test_read(self):
+        "frames return correct values when read using index"
+        # Frame will be 0001 0010 0011 0100 0101 0110
+        # Index:        3210 9876 5432 1098 7654 3210
+        f=frame.Frame(24, (0x12,0x34,0x56))
+        self.assertEqual(f[0], False)
+        self.assertEqual(f[1], True)
+        self.assertEqual(f[20], True)
+        self.assertRaises(IndexError, lambda: f[24])
+        self.assertRaises(IndexError, lambda: f[-1])
+        self.assertRaises(TypeError, lambda: f["wibble"])
+        self.assertRaises(TypeError, lambda: f[3:0:2])
+        self.assertRaises(IndexError, lambda: f[24:20])
+        self.assertEqual(f[3:0], 6)
+        self.assertEqual(f[7:4], 5)
+        self.assertEqual(f[4:7], 5)
+        self.assertEqual(f[23:20], 1)
+        self.assertEqual(f[2:0], 6)
+        self.assertEqual(f[0:0], 0)
+        self.assertEqual(f[1:1], 1)
+        self.assertEqual(f[2:1], 3)
+        self.assertEqual(f[17:10], int("10001101",2))
+
+    def test_write(self):
+        f=frame.Frame(24)
+        f[1] = True
+        self.assertEqual(f[23:0], 2)
+        f[2] = True
+        self.assertEqual(f[23:0], 6)
+        f[1] = False
+        self.assertEqual(f[23:0], 4)
+        f[0] = "yes"
+        self.assertEqual(f[23:0], 5)
+        f[2] = 0
+        self.assertEqual(f[23:0], 1)
+        with self.assertRaises(IndexError):
+            f[24]=True
+        with self.assertRaises(IndexError):
+            f[-24]=True
+        with self.assertRaises(TypeError):
+            f["wobble"]=False
+        with self.assertRaises(TypeError):
+            f[4:3:2]=2
+        # Test large frame that stores data as a long
+        f=frame.Frame(256)
+        f[200]=True
+        f[202]=True
+        self.assertEqual(f[202:199], 10)
+        f=frame.Frame(24)
+        f[7:4]=0xf
+        self.assertEqual(f[23:0],0xf0)
+        f[4:4]=0
+        self.assertEqual(f[23:0],0xe0)
+        f[20:23]=0xa
+        self.assertEqual(f[23:0],0xa000e0)
+        with self.assertRaises(ValueError):
+            f[3:0]=0x10
+        with self.assertRaises(TypeError):
+            f[20:20]="wibble"
+        with self.assertRaises(IndexError):
+            f[24:20]=10
+        with self.assertRaises(ValueError):
+            f[7:4]=-2
+
+    def test_add(self):
+        self.assertEquals(frame.Frame(16, 0x1234) + frame.Frame(16, 0x5678),
+                          frame.Frame(32, 0x12345678))
+        self.assertRaises(TypeError, lambda: frame.Frame(8,0xff) + 1)
+        self.assertRaises(TypeError, lambda: 1 + frame.Frame(8,0xff))
+
+    def test_as_integer(self):
+        self.assertEquals(frame.Frame(32,0x12345678).as_integer, 0x12345678)
+
+    def test_as_byte_sequence(self):
+        f=frame.Frame(29,0x12345678)
+        self.assertEquals(frame.Frame(29,f.as_byte_sequence), f)
+
+    def test_pack(self):
+        f=frame.Frame(28,0x2345678)
+        self.assertEquals(f.pack, b'\x02\x34\x56\x78')
+        f=frame.Frame(16,0xaa55)
+        self.assertEquals(f.pack, b'\xaa\x55')
+
+    def test_contains(self):
+        "frame __contains__ method works as expected"
+        self.assertTrue(True in frame.Frame(16, 0xaa55))
+        self.assertTrue(False in frame.Frame(16 ,0xaa55))
+        self.assertFalse(True in frame.Frame(16, 0))
+        self.assertFalse(False in frame.Frame(16, 0xffff))
+        self.assertFalse("wibble" in frame.Frame(16))
+
+    def test_unicode(self):
+        "frame objects return unicode from their __unicode__ method"
+        self.assertTrue(
+            isinstance(frame.Frame(123,0x12345).__unicode__(),
+                       unicode))
+
+if __name__ == "__main__":
+    unittest.main()
