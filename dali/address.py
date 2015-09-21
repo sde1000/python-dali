cce4a9f6c01d25e2a192e24375b873838fc3488b
Stephen Early <steve@greenend.org.uk>
2015-09-21 12:06:55 +0100

    Address support for 16- and 24-bit frames

--- dali/address.py
+++ dali/address.py
@@ -1,3 +1,27 @@
+"""Device addressing
+
+Addressing for control gear is described in IEC 62386-102 section 7.2.
+Forward frames can be addressed to one of 64 short addresses, one of
+16 group addresses, to all devices, or to all devices that do not have
+a short address assigned.
+
+Addressing for control devices is described in IEC 62386-103 section
+7.2.1.  Command frames can be addressed to one of 64 short addresses,
+one of 32 group addresses, to all devices, or to all devices that do
+not have a short address assigned.
+
+Addressing for event messages is described in IEC 62386-103 section
+7.2.2.  Decoding of event messages is currently not implemented.
+"""
+
+from __future__ import unicode_literals
+
+class IncompatibleFrame(Exception):
+    """Cannot set destination address in supplied frame"""
+    pass
+
+_bad_frame_length = IncompatibleFrame("Unsupported frame size")
+
 class AddressTracker(type):
     """Metaclass keeping track of all the types of Address we understand."""
 
@@ -11,6 +35,23 @@ class AddressTracker(type):
 class Address(object):
     """An address for one or more ballasts."""
     __metaclass__ = AddressTracker
+
+    @classmethod
+    def from_frame(cls, f):
+        """Return an Address object
+
+        If the frame has a destination address, return a suitable
+        Address object; otherwise return None.
+        """
+        if cls != Address:
+            return
+        for at in cls._addrtypes:
+            r = at.from_frame(f)
+            if r:
+                return r
+
+    def add_to_frame(self, f):
+        raise IncompatibleFrame("Cannot add unknown address to any frame")
 
     @classmethod
     def from_byte(cls, a):
@@ -31,11 +72,28 @@ class Address(object):
         return None
 
     def __unicode__(self):
-        return u"<no address>"
+        return "<no address>"
 
 
 class Broadcast(Address):
-    """All control gear connected to the network."""
+    """All control gear or devices connected to the network."""
+
+    @classmethod
+    def from_frame(cls, f):
+        if len(f) == 16:
+            if f[15:9] == 0x7f:
+                return cls()
+        elif len(f) == 24:
+            if f[16] and f[23:17] == 0x7f:
+                return cls()
+
+    def add_to_frame(self, f):
+        if len(f) == 16:
+            f[15:9] = 0x7f
+        elif len(f) == 24:
+            f[23:17] = 0x7f
+        else:
+            raise _bad_frame_length
 
     @property
     def addrbyte(self):
@@ -51,11 +109,32 @@ class Broadcast(Address):
         return isinstance(other, Broadcast)
 
     def __unicode__(self):
-        return u"<broadcast>"
+        return "<broadcast>"
 
 
 class BroadcastUnaddressed(Address):
-    """All control devices in the system that have no short address."""
+    """All unaddressed control gear or devices
+
+    All control gear or devices in the system that have no short
+    address assigned.
+    """
+
+    @classmethod
+    def from_frame(cls, f):
+        if len(f) == 16:
+            if f[15:9] == 0x7e:
+                return cls()
+        elif len(f) == 24:
+            if f[16] and f[23:17] == 0x7e:
+                return cls()
+
+    def add_to_frame(self, f):
+        if len(f) == 16:
+            f[15:9] = 0x7e
+        elif len(f) == 24:
+            f[23:17] = 0x7e
+        else:
+            raise _bad_frame_length
 
     @property
     def addrbyte(self):
@@ -71,18 +150,40 @@ class BroadcastUnaddressed(Address):
         return isinstance(other, BroadcastUnaddressed)
 
     def __unicode__(self):
-        return u"<broadcast unaddressed>"
+        return "<broadcast unaddressed>"
 
 
 class Group(Address):
-    """All ballasts that are members of the specified group."""
+    """All control gear or devices that are members of the specified group."""
 
     def __init__(self, group):
         if not isinstance(group, int):
             raise ValueError("group must be an integer")
-        if group < 0 or group > 15:
-            raise ValueError("group must be in the range 0..15")
+        if group < 0 or group > 31:
+            raise ValueError("group must be in the range 0..31")
         self.group = group
+
+    @classmethod
+    def from_frame(cls, f):
+        if len(f) == 16:
+            if f[15:13] == 0x4:
+                return cls(f[12:9])
+        elif len(f) == 24:
+            if f[16] and f[23:22] == 0x2:
+                return cls(f[21:17])
+
+    def add_to_frame(self, f):
+        if len(f) == 16:
+            if self.group > 15:
+                raise IncompatibleFrame(
+                    "Groups 16..31 are not supported in 16-bit forward frames")
+            f[15:13] = 0x4
+            f[12:9] = self.group
+        elif len(f) == 24:
+            f[23:22] = 0x2
+            f[21:17] = self.group
+        else:
+            raise _bad_frame_length
 
     @property
     def addrbyte(self):
@@ -98,14 +199,18 @@ class Group(Address):
         return isinstance(other, Group) and other.group == self.group
 
     def __unicode__(self):
-        return u"<group %d>" % self.group
+        return "<group %d>" % self.group
 
 
 class Short(Address):
-    """The particular ballast that has this address.
+    """The control gear or device that has this address.
 
-    In a correctly configured DALI network, no more than one ballast will
-    have a particular short address.
+    In a correctly configured DALI network, no more than one control
+    gear will have a particular short address, and no more than one
+    control device will have a particular short address.  The short
+    address spaces for control gear and control devices are separate;
+    it is legal for a control gear and control device to share a short
+    address.
     """
 
     def __init__(self, address):
@@ -114,6 +219,25 @@ class Short(Address):
         if address < 0 or address > 63:
             raise ValueError("address must be in the range 0..63")
         self.address = address
+
+    @classmethod
+    def from_frame(cls, f):
+        if len(f) == 16:
+            if not f[15]:
+                return cls(f[14:9])
+        elif len(f) == 24:
+            if f[16] and not f[23]:
+                return cls(f[22:17])
+
+    def add_to_frame(self, f):
+        if len(f) == 16:
+            f[15] = False
+            f[14:9] = self.address
+        elif len(f) == 24:
+            f[23] = False
+            f[22:17] = self.address
+        else:
+            raise _bad_frame_length
 
     @property
     def addrbyte(self):
@@ -129,9 +253,7 @@ class Short(Address):
         return isinstance(other, Short) and other.address == self.address
 
     def __unicode__(self):
-        return u"<address %d>" % self.address
-
+        return "<address %d>" % self.address
 
 from_byte = Address.from_byte
-
-__all__ = ["Address", "Broadcast", "Group", "Short", "from_byte"]
+from_frame = Address.from_frame
