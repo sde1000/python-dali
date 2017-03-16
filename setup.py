66f0dec88fbf9a8d2fd06fc48a451c9846630f67
Diogo Gomes <dgomes@ua.pt>
2017-03-16 09:57:38 +0000

    corrections

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@
 
 from distutils.core import setup
 
-#try:
-#    import pypandoc
-#    long_description = pypandoc.convert('README.md', 'rst')
-#except(IOError, ImportError):
-#    long_description = open('README.md').read()
+try:
+    import pypandoc
+    longdesc = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    longdesc = open('README.md').read()
 
 setup(
     name='python-dali',
-    version='0.3',
+    version='0.3b',
     description='Interface to DALI lighting systems',
     author='Stephen Early',
     author_email='steve@assorted.org.uk',
     url='https://github.com/sde1000/python-dali',
     download_url='https://github.com/dgomes/python-dali/archive/python-dali-0.3.tar.gz',
+    long_description=longdesc,
     packages=[
         'dali',
         'dali.device',
@@ -23,7 +24,7 @@ setup(
         'dali.gear'
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2',
