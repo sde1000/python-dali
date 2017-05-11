3818c565c8dc380b08cf1e67d548dfe7aed65f23
Robert Niederreiter <office@squarewave.at>
2017-05-11 15:33:02 +0200

    Introduce topic related base exceptions and derive concrete exceptions from it

--- dali/exceptions.py
+++ dali/exceptions.py
@@ -1,51 +1,91 @@
 from __future__ import unicode_literals
 
 
-class IncompatibleFrame(Exception):
+###############################################################################
+# general
+###############################################################################
+
+class DALIError(Exception):
+    """Base exception for DALI related errors."""
+
+
+###############################################################################
+# address
+###############################################################################
+
+class AddressError(DALIError):
+    """Base Exception for address related errors."""
+
+
+class IncompatibleFrame(AddressError):
     """Cannot set destination address in supplied frame"""
 
 
-class DuplicateDevice(Exception):
-    """Attempt to add more than one device with the same short address
-    to a bus.
-    """
+###############################################################################
+# command
+###############################################################################
+
+class CommandError(DALIError):
+    """Base Exception for command related errors."""
 
 
-class BadDevice(Exception):
+class MissingResponse(CommandError):
+    """Response was absent where a response was expected."""
+
+
+class ResponseError(CommandError):
+    """Response had unexpected framing error."""
+
+
+###############################################################################
+# bus
+###############################################################################
+
+class BusError(DALIError):
+    """Base Exception for bus related errors."""
+
+
+class BadDevice(BusError):
     """Device with invalid attributes."""
 
 
-class DeviceAlreadyBound(Exception):
+class DeviceAlreadyBound(BusError):
     """Attempt to add a device to a bus that is already bound to a
     different bus.
     """
 
 
-class NotConnected(Exception):
+class DuplicateDevice(BusError):
+    """Attempt to add more than one device with the same short address
+    to a bus.
+    """
+
+
+class NoFreeAddress(BusError):
+    """An unused short address was required but none was available."""
+
+
+class NotConnected(BusError):
     """A connection to the DALI bus is required to complete this
     operation, but the bus is not connected.
     """
 
 
-class NoFreeAddress(Exception):
-    """An unused short address was required but none was available."""
-
-
-class ProgramShortAddressFailure(Exception):
+class ProgramShortAddressFailure(BusError):
     """A device did not accept programming of its short address."""
 
     def __init__(self, address):
         self.address = address
 
 
-class MissingResponse(Exception):
-    """Response was absent where a response was expected."""
+###############################################################################
+# driver
+###############################################################################
+
+class DriverError(DALIError):
+    """Base Exception for driver related errors."""
 
 
-class ResponseError(Exception):
-    """Response had unexpected framing error."""
-
-
-class CommunicationError(Exception):
-    """Exception raised in case of communication error with daliserver.
+class CommunicationError(DriverError):
+    """Exception raised in case of communication error with backend.
     """
