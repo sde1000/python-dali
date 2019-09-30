ada91ef82e8b453fa10e47ba8359880262b6053f
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2019-09-30 22:41:58 +0200

    Refactor API and add unit testing
    Adds series of small unit tests. API adapted accordingly where it would
    make it easier to perform further testing.

--- dali/driver/unipi.py
+++ dali/driver/unipi.py
@@ -1,6 +1,5 @@
 from __future__ import print_function, unicode_literals
 
-import ctypes as C
 import logging
 from time import sleep
 
@@ -11,72 +10,29 @@ from pymodbus.client.sync import (
 
 from dali.driver.base import DALIDriver, SyncDALIDriver
 from dali.frame import BackwardFrame, ForwardFrame
+from dali.gear.general import Compare
 
-HWNAMES = (
-    "B-1000-1",
-    "E-8Di8Ro-1",
-    "E-14Ro-1",
-    "E-16Di-1",
-    "E-8Di8Ro-1_P-11DiMb485",
-    "E-14Ro-1_P-11DiR485-1",
-    "E-16Di-1_P-11DiR485-1",
-    "E-14Ro-1_U-14Ro-1",
-    "E-16Di-1_U-14Ro-1",
-    "E-14Ro-1_U-14Di-1",
-    "E-16Di-1_U-14Di-1",
-    "E-4Ai3Ao-1",
-)
 DA_OPT_TWICE = 0x8
-DA_OPT_17BIT = 0x4
-
-# debug logging related
-DRIVER_CONSTRUCT = 0x0
-DRIVER_EXTRACT = 0x1
-_exco_str = {DRIVER_CONSTRUCT: "CONSTRUCT", DRIVER_EXTRACT: "EXTRACT"}
-
-
-class fi(C.Union):
-    _fields_ = [("i", C.c_uint), ("f", C.c_float)]
-
-
-def reg2_float(r1, r2):
-    x = fi()
-    x.i = (r2 << 16) | r1
-    return x.f
-
-
-def float2reg(f):
-    x = fi()
-    x.f = f
-    return (x.i & 0xFFFF), (x.i >> 16)
-
-
-def u32i(low, high):
-    val = low | (high << 16)
-    if val <= 0x7FFFFFFF:
-        return val
-    return val - 0x100000000
-
-
-def i32u(val):
-    if val < 0:
-        val = val + 0x100000000
-    return (val & 0xFFFF, val >> 16)
-
-
-def u2i(val):
-    if val <= 0x7FFF:
-        return val
-    return val - 0x10000
-
-
-def i2u(val):
-    if val >= 0:
-        return val
-    return val + 0x10000
 
 
 class RemoteArm:
+    """Modbus interface for Unipi backend"""
+
+    HWNAMES = (
+        "B-1000-1",
+        "E-8Di8Ro-1",
+        "E-14Ro-1",
+        "E-16Di-1",
+        "E-8Di8Ro-1_P-11DiMb485",
+        "E-14Ro-1_P-11DiR485-1",
+        "E-16Di-1_P-11DiR485-1",
+        "E-14Ro-1_U-14Ro-1",
+        "E-16Di-1_U-14Ro-1",
+        "E-14Ro-1_U-14Di-1",
+        "E-16Di-1_U-14Di-1",
+        "E-4Ai3Ao-1",
+    )
+
     def __init__(self, host, unit=1, baud=19200, timeout=1):
         if host.startswith("/"):
             self.pymc = pySerial(
@@ -86,41 +42,33 @@ class RemoteArm:
             self.pymc = pyRtu(host=host, port=502)
         self.unit = unit
 
-    def set_speed(self, x):
-        pass
-
     def close(self):
         self.pymc.close()
 
-    def write_regs(self, reg, values, unit=-1):
+    def write_regs(self, reg, values, unit=None):
         try:
-            len(values)
-            return self.pymc.write_registers(
-                reg, values, unit=self.unit if unit == -1 else unit
-            )
+            iter(values)
         except TypeError:
-            pass
-        return self.pymc.write_register(
-            reg, values, unit=self.unit if unit == -1 else unit
-        )
+            return self.pymc.write_register(reg, values, unit=unit or self.unit)
+        else:
+            return self.pymc.write_registers(reg, values, unit=unit or self.unit)
 
-    def read_regs(self, reg, cnt, unit=-1):
-        rr = self.pymc.read_holding_registers(
-            reg, cnt, unit=self.unit if unit == -1 else unit
-        )
+    def read_regs(self, reg, cnt, unit=None):
+        rr = self.pymc.read_holding_registers(reg, cnt, unit=unit or self.unit)
         return rr.registers
 
-    def write_coil(self, reg, val, unit=-1):
-        return self.pymc.write_coil(reg, val, unit=self.unit if unit == -1 else unit)
+    def write_coil(self, reg, val, unit=None):
+        return self.pymc.write_coil(reg, val, unit=unit or self.unit)
 
-    def reboot(self, unit=-1):
-        self.pymc.write_coil(1002, 1, unit=self.unit if unit == -1 else unit)
+    def reboot(self, unit=None):
+        self.pymc.write_coil(1002, 1, unit=unit or self.unit)
 
-    def showver(self):
+    def version_info(self):
+        """Dictionary representing the version information"""
         reg = self.read_regs(1000, 4)
         ver = reg[0]
         swver = ver >> 8
-        if (swver) < 4:
+        if swver < 4:
             hw = (ver & 0xFF) >> 4
             hwver = ver & 0xF
             swsub = 0
@@ -128,34 +76,33 @@ class RemoteArm:
             hw = reg[3] >> 8
             hwver = reg[3] & 0xFF
             swsub = ver & 0xFF
+
         try:
-            hwname = HWNAMES[hw]
+            hwname = self.HWNAMES[hw]
         except IndexError:
             hwname = "UNKNOWN"
-        print("SW v%d.%d HW %s v%d" % (swver, swsub, hwname, hwver))
-        print(
-            "hwconfig:        %d x DI, %d x DO, %d x AI, %d x AO, %d x RS485"
-            % (
-                reg[1] >> 8,
-                reg[1] & 0xFF,
-                reg[2] >> 8,
-                (reg[2] >> 4) & 0x0F,
-                reg[2] & 0x0F,
-            )
-        )
+
+        return {
+            "software_version": swver,
+            "software_sub_version": swsub,
+            "hardware_name": hwname,
+            "hardware_version": hwver,
+            "hardware_config": {
+                "DI": reg[1] >> 8,
+                "DO": reg[1] & 0xFF,
+                "AI": reg[2] >> 8,
+                "AO": (reg[2] >> 4) & 0x0F,
+                "RS485": reg[2] & 0x0F,
+            },
+        }
 
     def Vref(self):
-        # find reference voltage
+        """Find reference voltage"""
         rr = self.read_regs(5, 1)
         self.vr1 = rr[0]
         rr = self.read_regs(1009, 1)
         self.vr2 = rr[0]
         return 3.3 * self.vr2 / self.vr1
-
-
-def _log_frame(logger, exco, opt, ad, cm, st):
-    msg = "{}\n" "    Type: {}\n" "    Address: {}\n" "    Command: {}\n"
-    logger.info(msg.format(_exco_str[exco], hex(opt), hex(ad), hex(cm)))
 
 
 class DALINoResponse(object):
@@ -169,18 +116,21 @@ DALI_NO_RESPONSE = DALINoResponse()
 
 
 class UnipiDALIDriver(DALIDriver):
-    """``DALIDriver` implementation for UniPi DALI ModBus device.
-    """
+    """DALIDriver` implementation for UniPi DALI ModBus device"""
 
     debug = False
     logger = logging.getLogger("UnipiDALIDriver")
-    # next sequence number
-    _next_sn = 1
+
+    def __init__(self):
+        # next sequence number
+        self._next_sn = 0
+        super().__init__()
 
     def construct(self, command):
-        """Data expected by DALI Modbus
-             Reg_1 - 16bit - length, ...
-             Reg_2 - 16bit - address,command
+        """
+        Data expected by DALI Modbus
+        - Reg_1 - 16bit - length, ...
+        - Reg_2 - 16bit - address,command
         """
         frame = command.frame
         if len(frame) == 16:
@@ -188,8 +138,8 @@ class UnipiDALIDriver(DALIDriver):
             if command.is_config:
                 opt |= DA_OPT_TWICE
             ad, cm1 = frame.as_byte_sequence
-            reg2 = (ad << 8) | cm1
             reg1 = opt << 8
+            reg2 = (ad << 8) | cm1
         elif len(frame) == 24:
             opt = 0x3
             if command.is_config:
@@ -200,12 +150,10 @@ class UnipiDALIDriver(DALIDriver):
         else:
             raise ValueError("Unknown frame length: {}".format(len(frame)))
 
-        if self.debug:
-            _log_frame(self.logger, DRIVER_CONSTRUCT, opt, ad, cm1, 0)
         return (reg1, reg2)
 
     def extract(self, data):
-        """ ----- """
+        """Frames from modbus data"""
         if data[0] == 0x100:
             return BackwardFrame(data[1])
         if data[0] == 0x200:
@@ -215,12 +163,8 @@ class UnipiDALIDriver(DALIDriver):
 
     def _get_sn(self):
         """Get next sequence number."""
-        sn = self._next_sn
-        if sn > 255:
-            sn = self._next_sn = 1
-        else:
-            self._next_sn += 1
-        return sn
+        self._next_sn = self._next_sn % 255
+        return self._next_sn + 1
 
 
 class SyncUnipiDALIDriver(UnipiDALIDriver, SyncDALIDriver):
@@ -233,109 +177,51 @@ class SyncUnipiDALIDriver(UnipiDALIDriver, SyncDALIDriver):
         self._recvreg = 1 + 3 * bus
         self._fereg = 38 + int(bus / 2)
 
-    def send(self, command, timeout=None):
+    def _send_command(self, command):
+        """Push command out"""
         sleep(0.05)
         registers = self.construct(command)
         self.backend.write_regs(self._sendreg, registers)
         if command.is_config:
             sleep(0.05)
             self.backend.write_regs(self._sendreg, registers)
+
+    def _read_returning_frame(self, counter1):
+        """Check returning frames for replying to"""
+        counter2, reg1, reg2 = self.backend.read_regs(self._recvreg, 3)
+        if counter1 != counter2:
+            return self.extract((reg1, reg2))
+
+    def _reply_compare_frame(self, command, fe_counter1):
+        if hasattr(command, "_cmdval") and command._cmdval == Compare._cmdval:
+            fe_counter2, = self.backend.read_regs(self._fereg, 1)
+            return fe_counter1 != fe_counter2
+        return False
+
+    def send(self, command, timeout=None):
+        # Push out
+        self._send_command(command)
+
+        # If the command does not expect a response, we're done
+        if command._response is None:
+            return DALI_NO_RESPONSE
+
+        # Check for command responses
         counter1, = self.backend.read_regs(self._recvreg, 1)
         fe_counter1, = self.backend.read_regs(self._fereg, 1)
         try:
-            needsResponse = command._response is not None
-            frame = None
-            # print(str(command) + str(needsResponse))
-            if needsResponse:  # wait max 6*11ms
-                for i in range(6):
-                    sleep(0.011)
-                    counter2, reg1, reg2 = self.backend.read_regs(self._recvreg, 3)
-                    if counter1 != counter2:
-                        # print ("%d %04x %04x" % (i, reg1, reg2))
-                        frame = self.extract((reg1, reg2))
-                        if isinstance(frame, BackwardFrame):
-                            if command.response:
-                                return command.response(frame)
-                    if hasattr(command, "_cmdval") and command._cmdval == 0xA9:
-                        fe_counter2, = self.backend.read_regs(self._fereg, 1)
-                        if fe_counter1 != fe_counter2:
-                            return command.response(BackwardFrame(0xFF))
-                return command.response(None)
-        except Exception as E:
-            print(str(E))
+            for i in range(6):
+                sleep(0.011)
+
+                # Check for backward frames
+                frame = self._read_returning_frame(counter1)
+                if isinstance(frame, BackwardFrame) and command.response:
+                    return command.response(frame)
+
+                # Check whether the command is a Compare command
+                if self._reply_compare_frame(command, fe_counter1):
+                    return command.response(BackwardFrame(0xFF))
+            return command.response(None)
+        except Exception:
+            pass
         return DALI_NO_RESPONSE
-
-
-def _test_sync(logger, command, bus=0, arg=0):
-    print("Test sync driver")
-    driver = SyncUnipiDALIDriver(bus=bus)
-    driver.logger = logger
-    driver.debug = True
-    # driver.
-    print("Response: {}".format(driver.send(command)))
-    driver.backend.close()
-
-
-if __name__ == "__main__":
-    """Usage: python unipidali.py address value
-    """
-    from dali.gear.general import (
-        Off,
-        DAPC,
-        Up,
-        OnAndStepUp,
-        IdentifyDevice,
-        RecallMaxLevel,
-    )
-    from dali.gear.general import (
-        QueryStatus,
-        QueryDeviceType,
-        QueryActualLevel,
-        AddToGroup,
-    )
-    from dali.address import Short, Broadcast, Group
-    from dali.bus import Bus
-    import sys
-
-    bus_driver = SyncUnipiDALIDriver(bus=1)
-    logger = logging.getLogger("UnipiDALIDriver")
-    bus_driver.logger = logger
-    bus_driver.debug = True
-    light_bus = Bus("1_01", bus_driver)
-    # light_bus.assign_short_addresses()
-    # sys.exit(0)
-    # setup console logging
-    logger.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(
-        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    )
-    logger.addHandler(handler)
-
-    # sync interface
-    addr = Group(int(sys.argv[1]))
-    # command to send
-    # command = DAPC(addr, int(sys.argv[2]))
-    # _test_sync(logger, command)
-    command = QueryStatus(addr)
-    _test_sync(logger, command, int(sys.argv[2]))
-    # for i in range(20):
-    command = DAPC(addr, 10)
-    _test_sync(logger, command, int(sys.argv[2]))
-    # #    sleep(1.00)
-    # #   command = RecallMaxLevel(addr)
-    #  #  _test_sync(logger, command, int(sys.argv[2]))
-    #   # sleep(1.00)
-    # command = OnAndStepUp(addr)
-    # _test_sync(logger, command, int(sys.argv[2]))
-    # command = QueryDeviceType(Broadcast())
-    # _test_sync(logger, command)
-    # command = QueryLightSourceType(addr)
-    # _test_sync(logger, command)
-    # command = QueryActualLevel(Broadcast())
-    # _test_sync(logger, command)
-    # command = RecallMaxLevel(addr)
-    # command = RecallMinLevel(addr)
-    # command = DAPC(Broadcast(), 235)
-    # _test_sync(logger, command)
