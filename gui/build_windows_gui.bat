091bebea99b6741df8afddd16daed3fa7f13f9db
Hans Baumgartner <hasseb@nic.fi>
2019-10-29 00:06:05 +0200

    Modified Windows build script

--- gui/build_windows_gui.bat
+++ gui/build_windows_gui.bat
@@ -1,8 +1,8 @@
-REM in case of "RuntimeError: Could not find the hidapi shared library." add path of hidapi.dll to the PATH environment variable
-
 pyinstaller --add-binary hidapi.dll;. dali_gui.py
 pyinstaller --add-binary hidapi.dll;. --add-binary hasseb_icon.ico;. --icon hasseb_icon.ico -F --noconsole dali_gui.py
 copy hidapi.dll dist\
 copy hidapi.dll dist\dali_gui
 copy hasseb_icon.ico dist\
 copy hasseb_icon.ico dist\dali_gui
+
+"C:\Program Files (x86)\NSIS\makensis.exe" build_windows_install_package.nsi
