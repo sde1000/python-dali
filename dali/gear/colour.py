2a953e0acd01d405eb31e3a73df4d4f93674baec
Petr Ilgner <petr@ilgner.cz>
2019-11-29 01:36:34 +0100

    Added DT8 commands and responses.

--- dali/gear/colour.py
+++ dali/gear/colour.py
@@ -5,7 +5,8 @@ from dali import command
 from dali.gear.general import _StandardCommand
 from enum import Enum
 
-class QueryValueVariables(Enum):
+
+class QueryColourValueVariables(Enum):
     XCoordinate = 0
     YCoordinate = 1
     ColourTemperatureTC = 2
@@ -25,24 +26,81 @@ class QueryValueVariables(Enum):
 class _ColourCommand(_StandardCommand):
     _devicetype = 0x08
 
+
 class SetTemporaryXCoordinate(_ColourCommand):
     _cmdval = 0xE0
+
 
 class SetTemporaryYCoordinate(_ColourCommand):
     _cmdval = 0xE1
 
+
 class SetTemporaryRGBDimLevel(_ColourCommand):
     _cmdval = 0xEB
+
+
+class SetTemporaryWAFDimLevel(_ColourCommand):
+    _cmdval = 0xEC
+
 
 class SetTemporaryRGBWAFControl(_ColourCommand):
     _cmdval = 0xED
 
+
 class Activate(_ColourCommand):
     _cmdval = 0xE2
 
-class QueryGearFeatures(_ColourCommand):
+
+class QueryGearFeaturesStatusResponse(command.BitmapResponseBitDict):
+    """Retrive gear feature/status info byte from the gear:
+    Bit 0: automatic ativation; 0 = NO
+    Bit 1..5: reserved
+    Bit 6: automatic callibration supported; 0 = NOT SUPPORTED
+    Bit 7: automatic callibration recovery supported; 0 = NOT SUPPORTED
+    """
+    bits = {0: "auto activation enabled", 6: "auto calibration supported", 7: "auto calibration recovery supported"}
+
+
+class QueryGearFeaturesStatus(_ColourCommand):
     _cmdval = 0xF7
-    _response = command.NumericResponse
+    _response = QueryGearFeaturesStatusResponse
+
+
+class QueryColourStatusResponse(command.BitmapResponse):
+    """Retrive gear colour status information:
+    Bit 0: xy-coordinate colour point out of range; 0 = NO
+    Bit 1: colour temperature Tc out of range; 0 = NO
+    Bit 2: auto calibration running; 0 = NO
+    Bit 3: auto calibration successful; 0 = NO
+    Bit 4: colour type xy-coordinate active; 0 = NO
+    Bit 5: colour type colour temperature Tc active; 0 = NO
+    Bit 6: colour type primary N active; 0 = NO
+    Bit 7: colour type RGBWAF active; 0 = NO
+    """
+    bits = ["xy-coord point out of range", "Tc temperature out of range", "Auto calibration running",
+            "type xy-coord active", "type Tc temperature active", "type primary N active", "type RGBWAF active"]
+
+
+class QueryColourStatus(_ColourCommand):
+    _cmdval = 0xF8
+    _response = QueryColourStatusResponse
+
+
+class QueryColourTypeFeaturesResponse(command.BitmapResponse):
+    """Retrieve gear supported colour type features:
+
+    Bit 0: xy-coordinate
+    Bit 1: colour temperature Tc
+    Bit 2: primary N
+    Bit 2: RGBWAF
+    """
+    bits = ["xy-coordinate", "colour temperature Tc", "primary N", "RGBWAF"]
+
+
+class QueryColourTypeFeatures(_ColourCommand):
+    _cmdval = 0xF9
+    _response = QueryColourTypeFeaturesResponse
+
 
 class QueryColourValue(_ColourCommand):
     _cmdval = 0xFA
