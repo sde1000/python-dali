5795e1f22229b17c02f85de9c9dd5de9d0f67f3c
Ferdinand Keil <Ferdinand.Keil@ies.tu-darmstadt.de>
2020-04-21 21:44:13 +0200

    added documentation to memory values

--- dali/memory/energy.py
+++ dali/memory/energy.py
@@ -1,6 +1,7 @@
 from .location import MemoryLocation, MemoryType, MemoryValue, NumericValue, StringValue, ScaledNumericValue, LockableValueMixin
 
 class ActiveEnergy(ScaledNumericValue, LockableValueMixin):
+    """Active Energy in Wh"""
 
     unit = 'Wh'
 
@@ -16,6 +17,7 @@ class ActiveEnergy(ScaledNumericValue, LockableValueMixin):
     scale_location = MemoryLocation(bank=202, address=0x04, default=None, reset=None, type_=MemoryType.ROM)
     
 class ActivePower(ScaledNumericValue, LockableValueMixin):
+    """Active Power in W"""
 
     unit = 'W'
 
@@ -29,8 +31,9 @@ class ActivePower(ScaledNumericValue, LockableValueMixin):
     scale_location = MemoryLocation(bank=202, address=0x0b, default=None, reset=None, type_=MemoryType.ROM)
 
 class ApparentEnergy(ScaledNumericValue, LockableValueMixin):
+    """Apparent Energy in VAh"""
 
-    unit = 'Wh'
+    unit = 'VAh'
 
     locations = (
         MemoryLocation(bank=203, address=0x05, default=0x00, reset=None, type_=MemoryType.NVM_RO),
@@ -44,8 +47,9 @@ class ApparentEnergy(ScaledNumericValue, LockableValueMixin):
     scale_location = MemoryLocation(bank=203, address=0x04, default=None, reset=None, type_=MemoryType.ROM)
     
 class ApparentPower(ScaledNumericValue, LockableValueMixin):
+    """Apparent Power in VA"""
 
-    unit = 'W'
+    unit = 'VA'
 
     locations = (
         MemoryLocation(bank=203, address=0x0c, default=0x0d, reset=None, type_=MemoryType.RAM_RO),
@@ -57,6 +61,7 @@ class ApparentPower(ScaledNumericValue, LockableValueMixin):
     scale_location = MemoryLocation(bank=203, address=0x0b, default=None, reset=None, type_=MemoryType.ROM)
 
 class ActiveEnergyLoadside(ScaledNumericValue, LockableValueMixin):
+    """Loadside Active Energy in Wh"""
 
     unit = 'Wh'
 
@@ -72,6 +77,7 @@ class ActiveEnergyLoadside(ScaledNumericValue, LockableValueMixin):
     scale_location = MemoryLocation(bank=204, address=0x04, default=None, reset=None, type_=MemoryType.ROM)
     
 class ActivePowerLoadside(ScaledNumericValue, LockableValueMixin):
+    """Loadside Active Power in W"""
 
     unit = 'W'
 
@@ -85,30 +91,28 @@ class ActivePowerLoadside(ScaledNumericValue, LockableValueMixin):
     scale_location = MemoryLocation(bank=204, address=0x0b, default=None, reset=None, type_=MemoryType.ROM)
 
 if __name__ == '__main__':
+    import argparse
     from dali.driver.quaddali import SyncQuadDALIUSBDriver
     from dali.address import Short
 
-    iface = SyncQuadDALIUSBDriver(port='/dev/ttyS24')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', help='repeat query n times', type=int, default=1)
+    parser.add_argument('-s', help='short address of the device that is queried', type=int, default=0)
+    parser.add_argument('port', help='serial port to connect to', type=str)
+    args = parser.parse_args()
 
-    print('"',
-        ActiveEnergy.retrieve(iface, Short(0))
-    ,'"')
-    print('"',
-        ActivePower.retrieve(iface, Short(0))
-    ,'"')
-    print()
-    print('"',
-        ApparentEnergy.retrieve(iface, Short(0))
-    ,'"')
-    print('"',
-        ApparentPower.retrieve(iface, Short(0))
-    ,'"')
-    print()
-    print('"',
-        ActiveEnergyLoadside.retrieve(iface, Short(0))
-    ,'"')
-    print('"',
-        ActivePowerLoadside.retrieve(iface, Short(0))
-    ,'"')
+    iface = SyncQuadDALIUSBDriver(port=args.port)
+
+    for i in range(args.n):
+        print(f'Run #{i+1}')
+        print(f'ActiveEnergy: {ActiveEnergy.retrieve(iface, Short(args.s)):.3f} {ActiveEnergy.unit}')
+        print(f'ActivePower: {ActivePower.retrieve(iface, Short(args.s)):.3f} {ActivePower.unit}')
+        print(f'ApparentEnergy: {ApparentEnergy.retrieve(iface, Short(args.s)):.3f} {ApparentEnergy.unit}')
+        print(f'ApparentPower: {ApparentPower.retrieve(iface, Short(args.s)):.3f} {ApparentPower.unit}')
+        print(f'ActiveEnergyLoadside: {ActiveEnergyLoadside.retrieve(iface, Short(args.s)):.3f} ' + \
+            f'{ActiveEnergyLoadside.unit}')
+        print(f'ActivePowerLoadside: {ActivePowerLoadside.retrieve(iface, Short(args.s)):.3f} ' + \
+            f'{ActivePowerLoadside.unit}')
+        print()
 
     iface.backend.close()
