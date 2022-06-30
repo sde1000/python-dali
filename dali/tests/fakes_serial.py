3b1c405470db86fe614e64737630fff3811677e0
Sean Lanigan <sl@wallace.com.au>
2022-06-30 14:35:05 +1000

    Add 'fakes' and tests for serial driver
    It is helpful when testing other parts of the library to be able to use
    a 'fake' device, which doesn't actually rely on any hardware.
    DriverSerialDummy implements such a fake device, which writes any DALI
    commands to a log file rather than over a real bus. There is limited
    support for emulating DALI control gear, to use as a testing target.
    
    Unit tests have also been added, to test the dummy implementation -
    which by extension also performs at least some basic tests on the
    DriverSerialBase class.

---
+++ dali/tests/fakes_serial.py
@@ -0,0 +1,331 @@
+"""
+fakes_serial.py - A mock implementation of a serial driver, for testing without relying on hardware
+
+
+This file is part of python-dali.
+
+python-dali is free software: you can redistribute it and/or modify it under the terms of the GNU
+Lesser General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+
+
+
+DriverSerialDummy provides a basic mock DALI bus, with fake devices connected to it. These fake
+devices can respond to a limited subset of DALI commands - useful in testing logic of other parts
+of the library.
+
+DriverSerialDummy instances are created using a path to a file, rather than a real serial device.
+This file is used to log all of the DALI commands which would be sent out over the DALI bus if
+using a "real" driver. During tests, the log file can be read to verify the expected command was
+sent.
+
+Three types of devices are emulated, DT6 LEDs, DT7 relays, and the Lunatone OEM-specific "Jalousie"
+relay (used for controlling blinds). The number of each dummy device to create is specified in a
+query string, after the path to the logfile, using the keys 'led', 'relay', and 'cover'.
+
+Example:
+```
+dummy_driver = DriverSerialDummy(uri="dummy:/tmp/dali0.txt?led=2&cover=2&relay=4")
+await dummy_driver.connect()
+```
+"""
+from __future__ import annotations
+
+import asyncio
+import logging
+from datetime import datetime
+from typing import Optional, Union
+from urllib.parse import ParseResult, parse_qs
+
+from dali import command, gear, memory
+from dali.driver.serial import DriverSerialBase
+from dali.memory.location import MemoryBank
+from dali.tests import fakes as dali_fakes
+from dali.driver import trace_logging  # noqa: F401
+
+_LOG = logging.getLogger("dali.driver")
+
+
+class DriverSerialDummy(DriverSerialBase):
+    uri_scheme = "dummy"
+    hardware_delay = 0.005
+
+    class DummyBank0(dali_fakes.FakeMemoryBank):
+        bank = memory.info.BANK_0
+        initial_contents = [
+            0x7F,
+            None,
+            0x00,  # Memory bank 0 is last accessible
+            0x01,
+            0x1F,
+            0x71,
+            0xF7,
+            0x6B,
+            0xB1,  # GTIN 1234567654321
+            0x03,
+            0x02,  # Firmware version 3.2
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0xAA,
+            0xBB,
+            0xCC,
+            0xFF,  # Serial number
+            0x02,
+            0x01,  # Hardware version 2.1
+            0x08,
+            0x08,  # Parts 101 and 102 version 2.0
+            0xFF,  # Part 103 not implemented
+            0x00,  # 0 logical control device units
+            0x01,  # 1 logical control gear unit
+            0x00,  # This is control gear unit index 0
+        ]
+
+    class DummyJalousieBank0(dali_fakes.FakeMemoryBank):
+        bank = MemoryBank(0, 0x10, has_lock=False)
+        initial_contents = [
+            0x7F,
+            None,
+            0x02,  # Memory bank 2 is last accessible
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,  # GTIN 0 (for some reason Lunatone don't support this)
+            0x02,
+            0x04,  # Firmware version 2.4
+            0xCC,
+            0xBB,
+            0xAA,
+            0x00,  # Serial number
+            0x01,  # Gear Unit Count
+            0x01,  # Gear Unit Index
+            0x00,
+            0x00,
+            0x02,
+            0x01,  # Hardware version 2.1
+            0x08,
+            0x01,  # The Jalousie uses DALI-1
+            0x00,
+        ]
+
+    class DummyJalousieBank2(dali_fakes.FakeMemoryBank):
+        bank = MemoryBank(2, 0x22, has_lock=False)
+        initial_contents = [
+            34,  # Length of memory bank
+            None,
+            None,
+            76,  # Seems like this is the Lunatone DALI-1 identifier
+            117,
+            110,
+            97,
+            116,
+            111,
+            110,
+            101,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            5,  # Specific part ID for Jalousie
+            39,
+            65,
+            52,
+            None,
+        ]
+
+    def __init__(self, *, uri: Union[str, ParseResult]):
+        """
+        The DriverSerialDummy class will 'magically' create pretend devices, based on the query
+        string passed in through the URI. These dummy devices won't physically exist, but do
+        provide a way of testing higher-level functions without needing physical hardware. Any
+        interactions that would be sent to hardware are instead written to a logfile, created
+        locally at the path provided in the URI.
+
+        :param uri: A special URI (either string or ParseResult object) which tells the dummy
+        driver where to save its logfile, and how many dummy devices to create.
+        URI format example: "dummy:///path/to/logfile?led=4&cover=2&relay=2"
+        """
+        super().__init__(uri)
+        uri = self.uri
+
+        self._log_path = uri.path
+        query = parse_qs(uri.query, strict_parsing=True)
+        self._num_leds = 0
+        try:
+            self._num_leds = int(query["led"][0])
+        except (IndexError, KeyError):
+            _LOG.info(f"URI query does not specify any LEDs: '{uri.query}'")
+        self._num_covers = 0
+        self._cover_addrs: set[int] = set()
+        try:
+            self._num_covers = int(query["cover"][0])
+        except (IndexError, KeyError):
+            _LOG.info(f"URI query does not specify any Covers: '{uri.query}'")
+        self._num_relays = 0
+        try:
+            self._num_relays = int(query["relay"][0])
+        except (IndexError, KeyError):
+            _LOG.info(f"URI query does not specify any Relays: '{uri.query}'")
+
+        _LOG.debug(f"Initialising dummy driver with logfile path '{self._log_path}'")
+        _LOG.info(
+            f"Dummy instances to be created: {self._num_leds} LEDs, {self._num_covers} covers, "
+            f"{self._num_relays} relays"
+        )
+
+        # Create dummy 'devices'. All are addressed sequentially.
+        self._dummy_bus = dali_fakes.Bus([])
+        new_address = 0
+        for _ in range(self._num_leds):
+            self._dummy_bus.gear.append(
+                dali_fakes.Gear(
+                    new_address,
+                    devicetypes=[6],
+                    memory_banks=(DriverSerialDummy.DummyBank0,),
+                )
+            )
+            # Force the LSB of the device serial number to equal the address
+            self._dummy_bus.gear[new_address].memory_banks[0].contents[18] = new_address
+            new_address += 1
+        for _ in range(self._num_covers):
+            new_cover = dali_fakes.Gear(
+                new_address,
+                devicetypes=[0],
+                memory_banks=(
+                    DriverSerialDummy.DummyJalousieBank0,
+                    DriverSerialDummy.DummyJalousieBank2,
+                ),
+            )
+            # Emulating a Lunatone Jalousie, which uses scenes to control movement up/down
+            new_cover.scenes[1] = 254
+            new_cover.scenes[3] = 253
+            # The serial number has a different address for DALI-1 (0x0E instead of 0x12)
+            new_cover.memory_banks[0].contents[0x0E] = new_address
+            self._dummy_bus.gear.append(new_cover)
+            # Keep track of which addresses belong to covers, for special handling
+            self._cover_addrs.add(new_address)
+            new_address += 1
+        for _ in range(self._num_relays):
+            self._dummy_bus.gear.append(
+                dali_fakes.Gear(
+                    new_address,
+                    devicetypes=[7],
+                    memory_banks=(DriverSerialDummy.DummyBank0,),
+                )
+            )
+            # Force the LSB of the device serial number to equal the address
+            self._dummy_bus.gear[new_address].memory_banks[0].contents[18] = new_address
+            new_address += 1
+
+        if new_address >= 64:
+            raise RuntimeError("Cannot have more than 64 devices on a DALI bus")
+
+    @staticmethod
+    def _get_date() -> str:
+        return datetime.now().isoformat()
+
+    def _set_level_off(self, addr: int):
+        _LOG.info(f"Turning off A{addr}")
+        self._dummy_bus.gear[addr].level = 0
+
+    async def connect(self) -> None:
+        if self.is_connected:
+            _LOG.warning("'connect()' called but dummy driver already connected")
+            return
+        # The dummy driver doesn't actually do any real communication, all we need to do is check
+        # that the path we've been given can be opened
+        with open(self._log_path, mode="wt", encoding="utf-8") as log_file:
+            log_file.write(
+                f"\n{self._get_date()} Starting new DriverSerialDummy instance\n"
+            )
+        # The file will be automatically closed after the write is done, it's not quite as efficient
+        # as it could be to open it each time, but it's a bit safer and ensures a clean write
+        self._connected.set()
+
+    async def send(
+        self, msg: command.Command, in_transaction: bool = False
+    ) -> Optional[command.Response]:
+        """
+        The 'send()' method for the DriverSerialDummy class doesn't actually communicate with
+        anything external - it just tries to imitate hardware, at a very basic level. For now,
+        all that is implemented is:
+        * Short addressing (i.e. referring to a dummy device directly by its address)
+        * Broadcast addressing
+        * Brightness/Power Level (can be set via DAPC, RecallMaxLevel, RecallMinLevel, Off)
+        * Device Type
+
+        :param msg: The DALI message to send
+        :param in_transaction: Flag whether or not this 'send()' call is part of a transaction
+        (i.e. if this call is coming from the 'run_sequence()' method). If the flag is set then
+        the lock will not be acquired before sending.
+        :return: If a command being sent expects a response, and the type is supported by
+        DriverSerialDummy, then it will be returned by the 'send()' call. If more than one command
+        generates a response then only the last response will be returned. Otherwise, None.
+        """
+        # Only send if the driver is connected
+        if not self.is_connected:
+            _LOG.critical(f"Cannot send, driver is not connected: {self}")
+            raise IOError("Cannot send, driver is not connected")
+
+        # If multiple responses are requested in a list of messages to send, only the last response
+        # will be returned
+        response = None
+
+        # "Send" each message, one at a time, and figure out what the dummy response should be
+        if not in_transaction:
+            await self.transaction_lock.acquire()
+        try:
+            with open(self._log_path, mode="at", encoding="utf-8") as log_file:
+                # Write the message to the log file, emulating sending it over a DALI bus
+                log_file.write(f"{self._get_date()} {msg}\n")
+                # Because writing to a log file is much faster than writing over RS232, there
+                # is a short delay here to make the dummy act a bit more like a real DALI device
+                await asyncio.sleep(DriverSerialDummy.hardware_delay)
+                # Get the response from the dummy bus
+                response = self._dummy_bus.send(msg)
+
+                # Special handling for a Lunatone Jalousie dummy device
+                if hasattr(msg, "destination"):
+                    if hasattr(msg.destination, "address"):
+                        if isinstance(msg, gear.general.GoToScene):
+                            dst = msg.destination.address
+                            if dst in self._cover_addrs:
+                                _LOG.info(f"Scheduling dummy call to turn off A{dst}")
+                                asyncio.get_running_loop().call_later(
+                                    delay=10.0,
+                                    callback=lambda: self._set_level_off(addr=dst),
+                                )
+
+        finally:
+            if not in_transaction:
+                self.transaction_lock.release()
+
+        return response
+
+    async def wait_dali_rx(self) -> command.Command:
+        raise RuntimeError("'wait_dali_rx()' is not implemented for DriverSerialDummy")
---
+++ dali/tests/test_dummy.py
@@ -0,0 +1,183 @@
+"""
+test_dummy.py - A pytest test suite for the 'dummy' serial driver class
+
+
+This file is part of python-dali.
+
+python-dali is free software: you can redistribute it and/or modify it under the terms of the GNU
+Lesser General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+"""
+import os.path
+from typing import NamedTuple
+
+import py
+import pytest
+
+from dali.driver.serial import DriverSerialBase, DriverLubaRs232, drivers_map
+from dali.tests.fakes_serial import DriverSerialDummy
+from dali import address, gear
+from dali.sequences import QueryDeviceTypes
+
+
+class DummyDriverFixtureParts(NamedTuple):
+    # The type hint for 'log' seems odd, but is correct - refer to this doc page
+    # https://docs.pytest.org/en/latest/reference/reference.html?highlight=tmpdir#tmpdir
+    log: py._path.local.LocalPath
+    uri: str
+    driver: DriverSerialDummy
+
+
+@pytest.fixture
+def make_dummy_driver(tmpdir):
+    """
+    A test fixture for creating an instance of a dummy driver
+    """
+
+    def _dummy_driver(
+        suffix: str = "0", led: int = 6, cover: int = 4, relay: int = 2
+    ) -> DummyDriverFixtureParts:
+        log = tmpdir.join(f"dummylog_{suffix}.txt")
+        uri = f"dummy://{log}?led={led}&cover={cover}&relay={relay}"
+        driver = DriverSerialDummy(uri=uri)
+
+        return DummyDriverFixtureParts(log=log, uri=uri, driver=driver)
+
+    return _dummy_driver
+
+
+@pytest.fixture
+def dummy_driver(make_dummy_driver) -> DummyDriverFixtureParts:
+    """
+    A test fixture that creates a basic instance of a test driver, using make_dummy_driver
+    """
+    return make_dummy_driver()
+
+
+def test_base_init_block():
+    """
+    Makes sure that the DriverSerialBase class cannot be initialised as a standalone object. It
+    needs to be used as a subclass.
+    """
+    with pytest.raises(RuntimeError):
+        DriverSerialBase(uri="")
+
+
+def test_drivers_map():
+    """
+    Tests the mapping from URI schemes to driver classes
+    """
+    drivers = drivers_map()
+    assert drivers["dummy"] == DriverSerialDummy
+    assert drivers["luba232"] == DriverLubaRs232
+
+
+def test_dummy_init_good(tmp_path):
+    """
+    Tests a dummy driver is initialised with the correct dummy devices
+    """
+    log = os.path.join(tmp_path, "dummylog.txt")
+    test_uri = f"dummy://{log}?led=2&cover=2&relay=4"
+    dummy_driver = DriverSerialDummy(uri=test_uri)
+
+    assert dummy_driver._num_leds == 2
+    assert dummy_driver._num_covers == 2
+    assert dummy_driver._num_relays == 4
+
+
+def test_dummy_init_bad_scheme(tmp_path):
+    """
+    Tests a dummy driver cannot be initialised with a bad URI scheme
+    """
+    log = os.path.join(tmp_path, "dummylog.txt")
+    test_uri = f"dmy://{log}?led=2&cover=2&relay=4"
+    with pytest.raises(TypeError):
+        DriverSerialDummy(uri=test_uri)
+
+
+def test_dummy_init_no_led(tmp_path):
+    """
+    Tests a dummy driver can be initialised without any dummy LEDs
+    """
+    log = os.path.join(tmp_path, "dummylog.txt")
+    test_uri = f"dummy://{log}logfile?cover=2&relay=4"
+    dummy_driver = DriverSerialDummy(uri=test_uri)
+
+    assert dummy_driver._num_leds == 0
+    assert dummy_driver._num_covers == 2
+    assert dummy_driver._num_relays == 4
+
+
+def test_dummy_init_no_cover(tmp_path):
+    """
+    Tests a dummy driver can be initialised without any dummy covers
+    """
+    log = os.path.join(tmp_path, "dummylog.txt")
+    test_uri = f"dummy://{log}?led=2&relay=4"
+    dummy_driver = DriverSerialDummy(uri=test_uri)
+
+    assert dummy_driver._num_leds == 2
+    assert dummy_driver._num_covers == 0
+    assert dummy_driver._num_relays == 4
+
+
+def test_dummy_init_no_relay(tmp_path):
+    """
+    Tests a dummy driver can be initialised without any dummy relays
+    """
+    log = os.path.join(tmp_path, "dummylog.txt")
+    test_uri = f"dummy://{log}?cover=2&led=4"
+    dummy_driver = DriverSerialDummy(uri=test_uri)
+
+    assert dummy_driver._num_leds == 4
+    assert dummy_driver._num_covers == 2
+    assert dummy_driver._num_relays == 0
+
+
+@pytest.mark.asyncio
+async def test_dummy_write_1(dummy_driver):
+    await dummy_driver.driver.connect()
+    await dummy_driver.driver.send(gear.general.DAPC(address.Short(1), 254))
+    assert "ArcPower(<address 1>,254)" in dummy_driver.log.read()
+
+
+@pytest.mark.asyncio
+async def test_dummy_write_2(dummy_driver):
+    await dummy_driver.driver.connect()
+    await dummy_driver.driver.send(gear.general.DAPC(address.Broadcast(), 127))
+    assert "ArcPower(<broadcast>,127)" in dummy_driver.log.read()
+
+
+@pytest.mark.asyncio
+async def test_dummy_write_3(dummy_driver):
+    await dummy_driver.driver.connect()
+    await dummy_driver.driver.send(gear.general.GoToScene(address.Short(1), 11))
+    assert "GoToScene(<address 1>,11)" in dummy_driver.log.read()
+
+
+@pytest.mark.asyncio
+async def test_dummy_sequence_device_types(dummy_driver):
+    await dummy_driver.driver.connect()
+    # The default of dummy_driver has 12 addressed control gears
+    for ad in range(11):
+        short = address.Short(ad)
+        dev_types = await dummy_driver.driver.run_sequence(QueryDeviceTypes(short))
+        # LEDs, DT6, are created first by the dummy driver
+        if ad in range(0, 5):
+            assert len(dev_types) == 1
+            assert dev_types[0] == 6
+        # Next is the custom Lunatone Jalousie cover, DT0
+        elif ad in range(6, 9):
+            assert len(dev_types) == 1
+            assert dev_types[0] == 0
+        # Finally the relays, DT7, are created
+        elif ad in range(10, 11):
+            assert len(dev_types) == 1
+            assert dev_types[0] == 7
