sde1000/python-dali

commit 0cfddb7ccf41c1c78ec04ee413670851d5c9fb47

Add support for DALI 24-bit event messages
IEC 62386 part 103 defines "event" messages, which can be sent by
control devices to indicate various inputs or changes. This commit
introduces support for encoding and decoding these messages, as well as
a handful of new sequences for making it easier to work with them.

Support has been added for IEC 62386 part 301 "push button" devices,
further device types should be simple enough to add in later.
Unsupported types will be safely ignored by the library.

dali/device/helpers.py
@@ -1,0 +1,232 @@
+"""
+Helper functions and classes, used in various other places through the library
+"""
+from __future__ import annotations
+
+import types
+from typing import Generator, Iterable, Optional
+
+from dali.address import DeviceShort, InstanceNumber
+from dali.command import Command, Response
+from dali.device.general import (
+    QueryDeviceStatus,
+    QueryDeviceStatusResponse,
+    QueryInstanceEnabled,
+    QueryInstanceType,
+    QueryNumberOfInstances,
+)
+from dali.exceptions import MissingResponse, ResponseError
+from dali.frame import BackwardFrameError
+from dali.sequences import progress as seq_progress
+
+
+def check_bad_rsp(r: Response | None) -> bool:
+    """
+    Checks if a response is "bad", i.e. if it is missing, is an error, or does
+    not have a value when one is expected.
+
+    :param r: A Response object to check
+    :return: A boolean, True if the response is "bad"
+    """
+    if not r:
+        return True
+    if isinstance(r.raw_value, BackwardFrameError):
+        return True
+    try:
+        # The 'value' property raises an exception if a value is expected but
+        # not present (except for NumericResponse)
+        val = r.value
+        # A NumericResponse uses strings to indicate error state
+        if isinstance(val, str):
+            if "missing" in val:
+                return True
+            if "framing error" in val:
+                return True
+    except (MissingResponse, ResponseError):
+        return True
+
+    return False
+
+
+class DeviceInstanceTypeMapper:
+    """
+    Part 103 defines five different event message addressing schemes, as per
+    Table 8 of IEC 62386.103. These are: "Instance", "Device",
+    "Device/Instance", "Device Group" and "Instance Group". Four of these
+    only rely on data contained within the message itself, however the
+    "Device/Instance" addressing scheme does not include information in the
+    24 bits of the message to indicate the instance type - which means that
+    without further knowledge it is not possible to decode these messages.
+
+    DeviceInstanceTypeMapper stores a mapping from the address and instance
+    number of a device to the instance type, thus allowing a higher-level
+    system to decode "Device/Instance" messages.
+
+    One instance of DeviceInstanceTypeMapper should be created for each DALI
+    bus, and then re-used whenever decoding event messages from that bus
+    through the `from_frame()` method.
+    """
+
+    def __init__(self, initial=None):
+        """
+        Creates a new DeviceInstanceTypeMapper object, optionally with
+        preloaded mappings as defined by `initial`.
+
+        :param initial: A dict of data to preload into the mapping
+        """
+        self._mapping: dict[(int, int), int] = {}
+        if initial:
+            self._mapping = initial
+
+    @property
+    def mapping(self) -> dict[(int, int), int]:
+        return self._mapping
+
+    def autodiscover(
+        self, addresses: int | tuple[int, int] | Iterable[int] = (0, 63)
+    ) -> Generator[Command, Response, None]:
+        """
+        A generator sequence to scan a DALI bus for control device instances,
+        and query their types. This information is stored within this
+        DeviceInstanceTypeMapper, for use in decoding "Device/Instance" event
+        messages.
+
+        :param addresses: Optional specifier of which addresses to scan. Can
+        either be a single int, in which case all addresses from zero to that
+        value will be scanned; or can be a tuple in the form (start, end), in
+        which case all addresses between the provided values will be scanned;
+        or finally can be an iterable of ints in which case each address, in
+        the iterator will be scanned.
+        :return: A generator function, to use with e.g. `driver.run_sequence()`
+
+        Needs to be used through an appropriate driver, with `run_sequence()`,
+        for example:
+        ```
+        dev_inst_map = DeviceInstanceTypeMapper()
+        await driver.run_sequence(dev_inst_map.autodiscover())
+        ```
+        """
+
+        if isinstance(addresses, int):
+            addresses = (n for n in range(0, addresses))
+        elif isinstance(addresses, tuple) and len(addresses) == 2:
+            addresses = (n for n in range(addresses[0], addresses[1] + 1))
+
+        for addr_int in addresses:
+            addr = DeviceShort(addr_int)
+
+            # Check that the device exists and responds
+            rsp = yield QueryDeviceStatus(device=addr)
+            if check_bad_rsp(rsp):
+                continue
+            if isinstance(rsp, QueryDeviceStatusResponse):
+                # Make sure the status is OK
+                if (
+                    rsp.input_device_error
+                    or rsp.quiescent_mode_enabled
+                    or rsp.short_address_is_mask
+                    or rsp.reset_state
+                ):
+                    continue
+            else:
+                # If the response isn't QueryDeviceStatusResponse then
+                # something is wrong
+                continue
+
+            # Find out how many instances the device has
+            rsp = yield QueryNumberOfInstances(device=addr)
+            if check_bad_rsp(rsp):
+                continue
+            num_inst = rsp.value
+
+            # For each instance, check it is enabled and then query the type
+            for inst_int in range(num_inst):
+                inst = InstanceNumber(inst_int)
+                rsp = yield QueryInstanceEnabled(device=addr, instance=inst)
+                if check_bad_rsp(rsp):
+                    continue
+                if not rsp.value:
+                    # Skip if not enabled
+                    continue
+                rsp = yield QueryInstanceType(device=addr, instance=inst)
+                if check_bad_rsp(rsp):
+                    continue
+
+                yield seq_progress(
+                    message=f"A²{addr_int} I{inst_int} type: {rsp.value}"
+                )
+
+                # Add the type to the device/instance map
+                self.add_type(
+                    short_address=addr,
+                    instance_number=inst,
+                    instance_type=rsp.value,
+                )
+
+    def add_type(
+        self,
+        *,
+        short_address: DeviceShort | int,
+        instance_number: InstanceNumber | int,
+        instance_type: types.ModuleType | int,
+    ) -> None:
+        """
+        Adds a mapping from device address and instance number to instance
+        type, so that the "Device/Instance" addressing scheme can be used for
+        event messages. Using this method will enable Device/Instance
+        messages to be decoded properly, instead of returning the default
+        `AmbiguousInstanceType` The * barrier means that arguments must be
+        named, this is to avoid accidental ambiguity.
+
+        :param short_address: Integer or `DeviceShort` of the relevant address
+        :param instance_number: Integer or `InstanceNumber` of the relevant
+        instance number
+        :param instance_type: Either an integer corresponding to the instance
+        type, e.g. 1 for "Part 301", or the module name that implements the type
+         e.g. "device.pushbutton" (which internally has a property
+         `instance_type`)
+        :return: None
+        """
+        if isinstance(short_address, DeviceShort):
+            short_address = short_address.address
+        if isinstance(instance_number, InstanceNumber):
+            instance_number = instance_number.value
+        if hasattr(instance_type, "instance_type"):
+            instance_type = int(instance_type.instance_type)
+        else:
+            instance_type = int(instance_type)
+        self._mapping[(short_address, instance_number)] = instance_type
+
+    def get_type(
+        self,
+        *,
+        short_address: DeviceShort | int,
+        instance_number: InstanceNumber | int,
+    ) -> Optional[int]:
+        """
+        Looks up the instance type based on the short address and instance
+        number. Only works if this has previously been added through a call
+        to `add_type()`. The * barrier means that arguments must be named,
+        this is to avoid accidental ambiguity.
+
+        :param short_address: Integer or `DeviceShort` of the relevant address
+        :param instance_number: Integer or `InstanceNumber` of the relevant
+        instance number
+        :return: Either the instance type as an integer, or None
+        """
+        if isinstance(short_address, DeviceShort):
+            short_address = short_address.address
+        if isinstance(instance_number, InstanceNumber):
+            instance_number = instance_number.value
+        return self._mapping.get((short_address, instance_number), None)
+
+    def clear(self) -> None:
+        """
+        Unconditionally clears all mappings
+
+        :return: None
+        """
+        self._mapping = {}
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self._mapping})"

dali/tests/test_device_sequences.py
@@ -1,0 +1,253 @@
+import pytest
+
+from dali.address import DeviceShort, InstanceNumber
+from dali.command import NumericResponse, YesNoResponse
+from dali.device import pushbutton
+from dali.device.general import (
+    DTR0,
+    EventScheme,
+    QueryDeviceStatus,
+    QueryDeviceStatusResponse,
+    QueryEventFilterZeroToSeven,
+    QueryEventScheme,
+    QueryEventSchemeResponse,
+    SetEventFilter,
+)
+from dali.device.helpers import DeviceInstanceTypeMapper, check_bad_rsp
+from dali.device.pushbutton import InstanceEventFilter as EventFilter_pb
+from dali.device.sequences import (
+    QueryEventFilters,
+    SetEventFilters,
+    SetEventSchemes,
+)
+from dali.frame import BackwardFrame, BackwardFrameError
+from dali.tests import fakes
+
+
+def test_check_bad_rsp_none():
+    # A 'None' is always a bad response
+    assert check_bad_rsp(None)
+
+
+def test_check_bad_rsp_yes():
+    # A 'Yes' is always a good response
+    assert not check_bad_rsp(YesNoResponse(BackwardFrame(0xFF)))
+
+
+def test_check_bad_rsp_numeric():
+    # A numeric is always a good response, even if 0
+    assert not check_bad_rsp(NumericResponse(BackwardFrame(0xFF)))
+    assert not check_bad_rsp(NumericResponse(BackwardFrame(0x00)))
+
+
+def test_check_bad_rsp_missing():
+    # A numeric response expects a value
+    assert check_bad_rsp(NumericResponse(None))
+
+
+def test_check_bad_rsp_framing_error():
+    # A numeric response expects a value
+    assert check_bad_rsp(NumericResponse(BackwardFrameError(0xFF)))
+
+
+@pytest.fixture
+def fakes_bus():
+    return fakes.Bus(
+        [
+            fakes.Device(DeviceShort(0), memory_banks=(fakes.FakeBank0,)),
+            fakes.Device(DeviceShort(1), memory_banks=(fakes.FakeBank0,)),
+            fakes.Device(DeviceShort(2), memory_banks=(fakes.FakeBank0,)),
+        ]
+    )
+
+
+def test_device_autodiscover_good(fakes_bus):
+    dev_inst_map = DeviceInstanceTypeMapper()
+    fakes_bus.run_sequence(dev_inst_map.autodiscover())
+    # There are 4 instances on each fake device, and 3 devices, so 12 in total
+    assert len(dev_inst_map.mapping) == 12
+
+
+class DeviceFakeError(fakes.Device):
+    # Refer to Table 15 of Part 103, status bit 0 is "inputDeviceError"
+    _device_status = 0b00000001
+
+
+def test_device_autodiscover_skip_bad(fakes_bus):
+    dev_inst_map = DeviceInstanceTypeMapper()
+    # Add an extra fake device, one with an error bit set
+    fakes_bus.gear.append(
+        DeviceFakeError(DeviceShort(3), memory_banks=(fakes.FakeBank0,))
+    )
+
+    # The input device error bit should show up
+    rsp = fakes_bus.send(QueryDeviceStatus(DeviceShort(3)))
+    assert isinstance(rsp, QueryDeviceStatusResponse)
+    assert rsp.input_device_error
+
+    fakes_bus.run_sequence(dev_inst_map.autodiscover())
+    # The device in error mode shouldn't have had its instances counted
+    assert len(dev_inst_map.mapping) == 12
+
+
+class DeviceNoInstances(fakes.Device):
+    _instances = []
+
+
+def test_device_autodiscover_skip_no_instances(fakes_bus):
+    dev_inst_map = DeviceInstanceTypeMapper()
+    # Add an extra fake device, one with an error bit set
+    fakes_bus.gear.append(
+        DeviceNoInstances(DeviceShort(3), memory_banks=(fakes.FakeBank0,))
+    )
+
+    fakes_bus.run_sequence(dev_inst_map.autodiscover())
+    # One device has no instances to count
+    assert len(dev_inst_map.mapping) == 12
+
+
+def test_device_query_event_scheme(fakes_bus):
+    rsp = fakes_bus.send(QueryEventScheme(DeviceShort(1), InstanceNumber(1)))
+    assert isinstance(rsp, QueryEventSchemeResponse)
+    assert rsp.value == EventScheme.device_instance
+
+
+def test_device_set_event_schemes(fakes_bus):
+    rsp = fakes_bus.send(QueryEventScheme(DeviceShort(2), InstanceNumber(3)))
+    assert isinstance(rsp, QueryEventSchemeResponse)
+    assert rsp.value == EventScheme.device_instance
+
+    rsp = fakes_bus.run_sequence(
+        SetEventSchemes(DeviceShort(2), InstanceNumber(3), EventScheme.device)
+    )
+    assert isinstance(rsp, QueryEventSchemeResponse)
+    assert rsp.value == EventScheme.device
+
+
+def test_device_set_event_filters(fakes_bus):
+    rsp = fakes_bus.run_sequence(
+        QueryEventFilters(DeviceShort(1), InstanceNumber(2), pushbutton)
+    )
+    assert isinstance(rsp, EventFilter_pb)
+    assert rsp.value == 0
+
+    rsp = fakes_bus.run_sequence(
+        SetEventFilters(
+            DeviceShort(1),
+            InstanceNumber(2),
+            EventFilter_pb.short_press | EventFilter_pb.long_press_start,
+        )
+    )
+    assert isinstance(rsp, EventFilter_pb)
+    assert EventFilter_pb.short_press in rsp
+    assert EventFilter_pb.long_press_start in rsp
+    filter_rsp = rsp
+
+    rsp = fakes_bus.run_sequence(
+        QueryEventFilters(DeviceShort(1), InstanceNumber(2), EventFilter_pb)
+    )
+    assert isinstance(rsp, EventFilter_pb)
+    assert rsp == filter_rsp
+
+
+def test_event_filter_pushbutton_sequence_partial():
+    """
+    Confirms that the SetEventFilters sequence yields the expected
+    DALI message objects and sets the correct bit flags for DTR0 for
+    pushbutton instances
+    """
+    filter_to_set = (
+        EventFilter_pb.button_released
+        | EventFilter_pb.button_stuck_free
+        | EventFilter_pb.double_press
+    )
+    sequence = SetEventFilters(
+        device=DeviceShort(0),
+        instance=InstanceNumber(0),
+        filter_value=filter_to_set,
+    )
+    # The first message the sequence should send is DTR0
+    rsp = None
+    try:
+        cmd = sequence.send(rsp)
+    except StopIteration:
+        raise RuntimeError()
+    assert isinstance(cmd, DTR0)
+    assert cmd.frame.as_byte_sequence[2] == 0b10001001
+
+    # The second message should be SetEventFilter
+    try:
+        cmd = sequence.send(rsp)
+    except StopIteration:
+        raise RuntimeError()
+    assert isinstance(cmd, SetEventFilter)
+    assert cmd.destination == DeviceShort(0)
+    assert cmd.instance == InstanceNumber(0)
+
+    # The third message should be QueryEventFilterZeroToSeven
+    try:
+        cmd = sequence.send(rsp)
+    except StopIteration:
+        raise RuntimeError()
+    assert isinstance(cmd, QueryEventFilterZeroToSeven)
+
+    # The sequence should then just return whatever the response is
+    rsp = NumericResponse(BackwardFrame(0b10001001))
+    ret = None
+    try:
+        sequence.send(rsp)
+    except StopIteration as r:
+        ret = r.value
+    assert isinstance(ret, EventFilter_pb)
+    assert ret == filter_to_set
+    assert EventFilter_pb.button_released in ret
+    assert EventFilter_pb.button_stuck_free in ret
+    assert EventFilter_pb.double_press in ret
+    assert EventFilter_pb.short_press not in ret
+
+
+def test_event_filter_pushbutton_sequence_all():
+    filter_to_set = (
+        EventFilter_pb.double_press
+        | EventFilter_pb.long_press_start
+        | EventFilter_pb.button_pressed
+        | EventFilter_pb.short_press
+        | EventFilter_pb.long_press_repeat
+        | EventFilter_pb.button_released
+        | EventFilter_pb.long_press_stop
+        | EventFilter_pb.button_stuck_free
+        | EventFilter_pb.double_press  # Intentional duplicate
+    )
+    sequence = SetEventFilters(
+        device=DeviceShort(0),
+        instance=InstanceNumber(0),
+        filter_value=filter_to_set,
+    )
+    # The first message the sequence should send is DTR0
+    try:
+        cmd = sequence.send(None)
+    except StopIteration:
+        raise RuntimeError()
+    assert isinstance(cmd, DTR0)
+    assert cmd.frame.as_byte_sequence[2] == 0b11111111
+    # The previous test for the sequence adequately checks the remaining logic
+
+
+def test_event_filter_sequence_bad_type():
+    sequence = SetEventFilters(
+        device=DeviceShort(0),
+        instance=InstanceNumber(0),
+        filter_value="double_press",
+    )
+    # Using a string is not valid, it must be an enum object
+    with pytest.raises(TypeError):
+        sequence.send(None)
+
+    sequence = QueryEventFilters(
+        device=DeviceShort(0),
+        instance=InstanceNumber(0),
+        filter_type=EventFilter_pb.short_press,
+    )
+    # filter_type needs to be a type, not an instance
+    with pytest.raises(TypeError):
+        sequence.send(None)

.github/workflows/run-tests.yml
@@ -23,7 +23,7 @@ jobs:
           if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
       - name: Lint with flake8
         run: |
-          flake8 dali --exclude=driver --count --ignore=E741,F403,F405 --show-source --statistics --max-line-length=127
+          flake8 dali --exclude=driver --count --ignore=E741,F403,F405,W503 --show-source --statistics --max-line-length=127
           # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
           #flake8 dali --exclude=driver --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
       - name: Test with pytest

dali/address.py
@@ -101,8 +101,8 @@ class GearBroadcast(GearAddress):
         return "<broadcast (control gear)>"
 
 
-# Broadcast alias provided for legacy purposes, new code should avoid ambiguity by using
-# either GearBroadcast or DeviceBroadcast
+# Broadcast alias provided for legacy purposes, new code should avoid
+# ambiguity by using either GearBroadcast or DeviceBroadcast
 Broadcast = GearBroadcast
 
 
@@ -153,8 +153,9 @@ class GearBroadcastUnaddressed(GearAddress):
         return "<broadcast unaddressed (control gear)>"
 
 
-# BroadcastUnaddressed alias provided for legacy purposes, new code should avoid
-# ambiguity by using either GearBroadcastUnaddressed or DeviceBroadcastUnaddressed
+# BroadcastUnaddressed alias provided for legacy purposes, new code should
+# avoid ambiguity by using either GearBroadcastUnaddressed or
+# DeviceBroadcastUnaddressed
 BroadcastUnaddressed = GearBroadcastUnaddressed
 
 
@@ -213,8 +214,8 @@ class GearGroup(GearAddress):
         return f"<group (control gear) {self.group}>"
 
 
-# Group alias provided for legacy purposes, new code should avoid ambiguity by using
-# either GearGroup or DeviceGroup
+# Group alias provided for legacy purposes, new code should avoid ambiguity
+# by using either GearGroup or DeviceGroup
 Group = GearGroup
 
 
@@ -287,8 +288,8 @@ class GearShort(GearAddress):
         return f"<address (control gear) {self.address}>"
 
 
-# Short alias provided for legacy purposes, new code should avoid ambiguity by using
-# either GearShort or DeviceShort
+# Short alias provided for legacy purposes, new code should avoid ambiguity
+# by using either GearShort or DeviceShort
 Short = GearShort
 
 
@@ -342,6 +343,11 @@ from_frame = Address.from_frame
 class Instance:
     def __init__(self):
         raise NotImplementedError
+
+    @property
+    def value(self):
+        if hasattr(self, "_value"):
+            return self._value
 
     def add_to_frame(self, f):
         raise NotImplementedError

dali/command.py
@@ -221,7 +221,7 @@ class Command(metaclass=_CommandTracker):
         subs.append(subclass)
 
     @classmethod
-    def from_frame(cls, f, devicetype=0):
+    def from_frame(cls, f, devicetype=0, dev_inst_map=None):
         """Return a Command instance corresponding to the supplied frame.
 
         If the device type the command is intended for is known
@@ -230,6 +230,8 @@ class Command(metaclass=_CommandTracker):
 
         :parameter frame: a forward frame
         :parameter devicetype: type of device frame is intended for
+        :param dev_inst_map: An instance of DeviceInstanceTypeMapper to
+        assist with DALI events
 
         :returns: Return a Command instance corresponding to the
         frame.  Returns None if there is no match.
@@ -241,7 +243,7 @@ class Command(metaclass=_CommandTracker):
         subs = cls._framesizes.get(len(f), [])
 
         for c in subs:
-            r = c.from_frame(f, devicetype=devicetype)
+            r = c.from_frame(f, devicetype=devicetype, dev_inst_map=dev_inst_map)
             if r:
                 return r
 

dali/device/__init__.py
@@ -4,3 +4,6 @@ This module declares commands, responses and event messages defined in
 part 103 ("General requirements - Control devices") and parts 3xx
 (particular requirements for control devices).
 """
+import dali.device.general
+import dali.device.sequences
+import dali.device.pushbutton  # noqa: F401

commit cb0909fc4af24757706d35daac033a331b5b3efb

DeviceInstanceMapper: do not skip devices with inputDeviceError
I have a bunch of non-pushbuttons switches connected to some of my
Lunatone DALI-2 MC pushbutton couplers. These devices unfortunately
set the "inputDeviceError" bit whenever any of its inputs is
closed/shorted for longer than T_stuck.

The 62386-301:2017 says (Table 4, page 15) that this timer is up to
255s, which is definitely not enough for a door sensor. The same
standard then says that such errors should be propagated to bit 0 in the
"instanceErrorByte", which python-dali does not query. However, my
devices unfortunately also propagate this status into the
"inputDeviceError", and that means that the initial discovery of
instance numbers to instance types won't recognize these buttons at
all.

I was not able to find a reference in the standard whether any
"instanceError" propagates to the "inputDeviceError", but even if this
was an incorrect behavior, these devices exist, one can buy them, and
presumably using them as a kind-of persistent contact inputs still makes
sense; just to provide some data, I have:

- magnet and a reed switch for door status monitoring,
- a "permanent" switch for disabling automation at a hallway where our
  baby sometimes sleeps.

I think that both use cases make sense, and I would prefer python-dali
to not skip over some input devices which actually work just fine.

Fixes: 0cfddb7 Add support for DALI 24-bit event messages

dali/device/helpers.py
@@ -127,8 +127,7 @@ class DeviceInstanceTypeMapper:
             if isinstance(rsp, QueryDeviceStatusResponse):
                 # Make sure the status is OK
                 if (
-                    rsp.input_device_error
-                    or rsp.short_address_is_mask
+                    rsp.short_address_is_mask
                     or rsp.reset_state
                 ):
                     continue

dali/tests/test_device_sequences.py
@@ -73,7 +73,7 @@ class DeviceFakeError(fakes.Device):
     _device_status = 0b00000001
 
 
-def test_device_autodiscover_skip_bad(fakes_bus):
+def test_device_autodiscover_dont_skip_bad(fakes_bus):
     dev_inst_map = DeviceInstanceTypeMapper()
     # Add an extra fake device, one with an error bit set
     fakes_bus.gear.append(
@@ -86,8 +86,8 @@ def test_device_autodiscover_skip_bad(fakes_bus):
     assert rsp.input_device_error
 
     fakes_bus.run_sequence(dev_inst_map.autodiscover())
-    # The device in error mode shouldn't have had its instances counted
-    assert len(dev_inst_map.mapping) == 12
+    # The mere fact that a device has "inputDeviceError" set does not mean it is not usable
+    assert len(dev_inst_map.mapping) == 16
 
 
 class DeviceNoInstances(fakes.Device):