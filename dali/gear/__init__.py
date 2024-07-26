3f3d6eb524abacdb98cb3083fe72e9542553a64f
Jan Kundrát <jkt@jankundrat.com>
2024-07-26 16:12:28 +0200

    Add support for DT5 (0-10V conversion) devices
    Tested with Lunatone 86458508-AN. This one only appears to support
    switching between linear and logarithmic dimming curves, and some
    (undocumented) manufacturer's extensions. Support for pullup handling as
    well as switching between the 0-10V and 1-10V ranges is not there.

--- dali/gear/__init__.py
+++ dali/gear/__init__.py
@@ -7,5 +7,6 @@ extensions and control gear specific features).
 import dali.gear.general
 import dali.gear.emergency
 import dali.gear.incandescent
+import dali.gear.converter
 import dali.gear.colour
 import dali.gear.led  # noqa: F401
